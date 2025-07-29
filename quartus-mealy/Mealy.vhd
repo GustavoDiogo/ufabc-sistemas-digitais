889ba5b69d89c5b1142c0dc20046f7ce40fc8e92
Gustavo Silva <gustavo_guga_01@hotmail.com>
2025-07-28 22:10:23 -0300

    feat: fixes mealy machine in DE10-Lite

--- quartus-mealy/Mealy.vhd
+++ quartus-mealy/Mealy.vhd
@@ -5,7 +5,8 @@ entity Mealy is
     port (
         KEY  : in std_logic_vector(1 downto 0);  -- KEY(0): clock, KEY(1): reset
         SW   : in std_logic_vector(0 downto 0);  -- SW(0): entrada x
-        LEDR : out std_logic_vector(0 downto 0)  -- LEDR(0): saída z
+        LEDR : out std_logic_vector(0 downto 0); -- LEDR(0): saída z
+        HEX5, HEX4, HEX3, HEX2, HEX1, HEX0 : out std_logic_vector(0 to 6)
     );
 end Mealy;
 
@@ -13,6 +14,18 @@ architecture behavior of Mealy is
     type state_type is (S0, S1, S2, S3);
     signal state, next_state : state_type;
     signal z_internal : std_logic;
+
+    -- Mapeamento 7 segmentos (ativo baixo)
+	function char_to_7seg(c: character) return std_logic_vector is
+	begin
+		 case c is
+			  when 'F' => return "0111000"; -- segmentos 0,4,5,6
+			  when 'O' => return "0000001"; -- segmentos 0,1,2,3,4,5
+			  when 'I' => return "1111001"; -- segmentos 1,2
+			  when ' ' => return "1111111"; -- apagado
+			  when others => return "1111111";
+		 end case;
+	end;
 begin
     -- Processamento do estado
     process (KEY)
@@ -24,10 +37,10 @@ begin
         end if;
     end process;
 
-    -- Lógica de transição de estados e cálculo da saída
+    -- Lógica de transição e detecção da sequência
     process (state, SW)
     begin
-        z_internal <= '0';  -- valor padrão
+        z_internal <= '0';
         case state is
             when S0 =>
                 if SW(0) = '1' then
@@ -50,14 +63,32 @@ begin
             when S3 =>
                 if SW(0) = '1' then
                     next_state <= S1;
-                    z_internal <= '1';  -- Reconhecimento imediato!
+                    z_internal <= '1';  -- Ativa por 1 ciclo
                 else
                     next_state <= S0;
                 end if;
         end case;
     end process;
 
-    -- Conecta a saída à LEDR(0)
     LEDR(0) <= z_internal;
 
+    -- Displays com "  FOI "
+    process(z_internal)
+    begin
+        if z_internal = '1' then
+            HEX5 <= char_to_7seg(' ');
+            HEX4 <= char_to_7seg(' ');
+            HEX3 <= char_to_7seg('F');
+            HEX2 <= char_to_7seg('O');
+            HEX1 <= char_to_7seg('I');
+            HEX0 <= char_to_7seg(' ');
+        else
+            HEX5 <= (others => '1');
+            HEX4 <= (others => '1');
+            HEX3 <= (others => '1');
+            HEX2 <= (others => '1');
+            HEX1 <= (others => '1');
+            HEX0 <= (others => '1');
+        end if;
+    end process;
 end behavior;
