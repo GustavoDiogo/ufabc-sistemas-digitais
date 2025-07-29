ac30f70473c14ec9d0a066b4ab966bb5480a88fd
Gustavo Silva <gustavo_guga_01@hotmail.com>
2025-07-28 22:42:08 -0300

    feat: fixes moore machine to show states

--- quartus-moore/Moore.vhd
+++ quartus-moore/Moore.vhd
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 
 entity Moore is
     port (
-        KEY  : in std_logic_vector(1 downto 0);  -- KEY(0) = clk, KEY(1) = reset
-        SW   : in std_logic_vector(0 downto 0);  -- SW(0) = entrada x
-        LEDR : out std_logic_vector(0 downto 0); -- LEDR(0) = saída z
+        KEY  : in std_logic_vector(1 downto 0);
+        SW   : in std_logic_vector(0 downto 0);
+        LEDR : out std_logic_vector(0 downto 0);
         HEX5, HEX4, HEX3, HEX2, HEX1, HEX0 : out std_logic_vector(0 to 6)
     );
 end Moore;
@@ -14,19 +14,24 @@ architecture behavior of Moore is
     type state_type is (S0, S1, S2, S3, S4);
     signal state, next_state : state_type;
 
-    -- Função para converter caractere em padrão de 7 segmentos (ativo baixo)
     function char_to_7seg(c: character) return std_logic_vector is
     begin
         case c is
-            when 'F' => return "0111000"; -- segmentos 0,4,5,6
-            when 'O' => return "0000001"; -- segmentos 0,1,2,3,4,5
-            when 'I' => return "1111001"; -- segmentos 1,2
-            when ' ' => return "1111111"; -- apagado
+            when '0' => return "0000001";
+            when '1' => return "1001111";
+            when '2' => return "0010010";
+            when '3' => return "0000110";
+            when '4' => return "1001100";
+            when 'F' => return "0111000";
+            when 'O' => return "0000001";
+            when 'I' => return "1111001";
+            when 'S' => return "0100100";
+            when ' ' => return "1111111";
             when others => return "1111111";
         end case;
     end;
 begin
-    -- Processamento do estado
+    -- Atualização de estado
     process (KEY)
     begin
         if (KEY(1) = '0') then
@@ -73,27 +78,32 @@ begin
         end case;
     end process;
 
-    -- Saída LED
+    -- Saída z
     LEDR(0) <= '1' when state = S4 else '0';
 
-    -- Displays: "  FOI "
+    -- Displays
     process(state)
     begin
         if state = S4 then
-            HEX5 <= char_to_7seg(' ');
-            HEX4 <= char_to_7seg(' ');
+            HEX5 <= (others => '1');
+            HEX4 <= (others => '1');
             HEX3 <= char_to_7seg('F');
             HEX2 <= char_to_7seg('O');
             HEX1 <= char_to_7seg('I');
-            HEX0 <= char_to_7seg(' ');
+            HEX0 <= (others => '1');
         else
             HEX5 <= (others => '1');
             HEX4 <= (others => '1');
             HEX3 <= (others => '1');
             HEX2 <= (others => '1');
-            HEX1 <= (others => '1');
-            HEX0 <= (others => '1');
+            HEX1 <= char_to_7seg('S');
+            case state is
+                when S0 => HEX0 <= char_to_7seg('0');
+                when S1 => HEX0 <= char_to_7seg('1');
+                when S2 => HEX0 <= char_to_7seg('2');
+                when S3 => HEX0 <= char_to_7seg('3');
+                when S4 => HEX0 <= char_to_7seg('4'); -- opcional: pode ser apagado também
+            end case;
         end if;
     end process;
-
 end behavior;
